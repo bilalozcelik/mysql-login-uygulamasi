f36488e017e85ad9d52cde5cd103c5f991b08805
bilalozcelik <37147381+bilalozcelik@users.noreply.github.com>
2022-04-20 17:34:43 +0300

    Add files via upload

---
+++ login uygulaması/login uygulaması/Metot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace login_uygulaması
+{
+    public class Metot
+    {
+        #region kullanıcı kontrol
+        string conStr = "SERVER=localhost;DATABASE=testdb;UID=root;PWD=tbist.2022";
+        public int kullaniciKontrol(string kAd, string kSifre)
+        {
+            int sonuc = 0;
+            using (var con = new MySqlConnection(conStr))
+            {
+                using (var cmd = new MySqlCommand($"select k_adi,k_sifre from kullanicilar where k_adi='{kAd}' and k_sifre='{kSifre}'", con))
+                {
+                    try
+                    {
+                        cmd.Connection.Open();
+                        MySqlDataReader dtr = cmd.ExecuteReader();
+                        if (dtr.Read())
+                        {
+                            string d_k = dtr["k_adi"].ToString();
+                            string d_s = dtr["k_sifre"].ToString();
+                            if (d_k == kAd && d_s == kSifre)
+                            {
+                                sonuc = 1;
+                            }
+                            else
+                            {
+                                sonuc = 0;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        sonuc = 0;
+                    }
+                }
+                return sonuc;
+            }
+        }
+    }
+    #endregion
+
+}
---
+++ login uygulaması/login uygulaması/anaForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace login_uygulaması
+{
+    partial class anaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(anaForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblKullanici = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::login_uygulaması.Properties.Resources.user_png;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hoşgeldin";
+            // 
+            // lblKullanici
+            // 
+            this.lblKullanici.AutoSize = true;
+            this.lblKullanici.Location = new System.Drawing.Point(373, 58);
+            this.lblKullanici.Name = "lblKullanici";
+            this.lblKullanici.Size = new System.Drawing.Size(74, 17);
+            this.lblKullanici.TabIndex = 1;
+            this.lblKullanici.Text = "lblKullanici";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel1.Location = new System.Drawing.Point(290, 94);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(104, 17);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Oturumu Kapat";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // anaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 308);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblKullanici);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "anaForm";
+            this.Text = "Merhaba Kullanıcı";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.anaForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label lblKullanici;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
